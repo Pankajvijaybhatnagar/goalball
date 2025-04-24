c2b7db876237ef7a7174ea5bc2ce53b142bfda87
ankitjnvynr <ankitbkana@outlook.com>
2025-04-25 01:16:07 +0530

    Update event detail page to display featured image, event title, date range, description, and related events for enhanced user experience.

--- event.php
+++ event.php
@@ -42,28 +42,31 @@ $row = $res->fetch_assoc();
                     <div class="col-md-12 col-sm-12 col-lg-8">
                         <div class="post-detail w-100">
                             <div class="post-feat-img serv-detail-img brd-rd10 position-relative overflow-hidden w-100">
-                                <img class="img-fluid w-100" src="assets/images/resources/<?php echo $rows['featured_image'] ?>"
-                                    alt="Service Detail Image">
+                                <img class="img-fluid w-100"
+                                    src="uploads/<?php echo $row['image_folder_name'] ?>/<?php echo $row['featured_image'] ?>"
+                                    alt="GFI">
                                 <span class="brd-rd10 thm-bg serv-post-date position-absolute"></span>
                                 <span class="serv-post-authr position-absolute"><i class="fas fa-ball thm-clr"></i><a
                                         href="javasctipt:void()" title="">Goalball Federation of India</a></span>
                             </div>
                             <h1>
-                            <?php echo strtoupper( $row['title'])  ?>
+                                <?php echo strtoupper( $row['title'])  ?>
                             </h1>
-                            <h6>From : <?php echo date("F j, Y", strtotime($row['from_date']));  ?> to <?php echo date("F j, Y", strtotime($row['to_date']));  ?></h6>
-                               
+                            <h6>From : <?php echo date("F j, Y", strtotime($row['from_date']));  ?> to
+                                <?php echo date("F j, Y", strtotime($row['to_date']));  ?></h6>
+
                             <p style="text-align:justify;" class="mb-0">
                                 <?php
                                 echo $row['description']
                                 ?>
                             </p>
                             <blockquote class="blockquote text-center thm-bg brd-rd10">
-                                <i class="fas fa-quote-left d-block"></i>
-                                <p class="mb-0">dolor st amet, consetetur sadipscing elit dolore lorime smagna aliquyam.
+                                <i class="fas fa-map-marker d-block"></i>
+
+                                <p class="mb-0"> <?php echo $row['place'] ?>
                                 </p>
                             </blockquote>
-                            
+
                         </div>
                     </div>
                     <div class="col-md-6 col-sm-6 col-lg-4">
@@ -71,39 +74,31 @@ $row = $res->fetch_assoc();
                             <div class="widget-box v3 brd-rd10 bg-color6 overflow-hidden w-100">
                                 <h4 class="position-relative tit-shp thm-shp widget-title3">Other Events</h4>
                                 <div class="mini-posts-wrap w-100">
+                                    <?php
+                                        // print_r( $conn);
+
+                                        $sql = "SELECT * FROM `events`";
+                                        $res=$conn->query($sql);
+                                        ?>
+                                         <?php 
+                                        if($res->num_rows>0){
+
+                                        while($rows = $res->fetch_assoc()){
+                                        ?>
                                     <div class="mini-post-box d-flex flex-wrap align-items-center">
-                                        <a class="brd-rd5 overflow-hidden" href="product-detail.html" title=""><img
-                                                class="img-fluid" src="assets/images/resources/mini-product-img1-1.jpg"
+                                        <a class="brd-rd5 overflow-hidden" href="product-detail.html" title="">
+                                            <img class="img-fluid" width="80px" src="assets/images/logo2.png"
                                                 alt="Mini Product Image 1"></a>
                                         <div class="mini-post-info">
-                                            <h5 class="mb-0"><a href="product-detail.html" title="">Brown Vintage
-                                                    Gloves</a></h5>
+                                            <h5 class="mb-0"><a href="product-detail.html" title=""><?php echo strtoupper( $rows['title'])  ?></a></h5>
                                             <span
-                                                class="price scndry-clr d-block"><ins><small>$</small>85.00</ins><del><small>$</small>45.00</del></span>
+                                                class="price scndry-clr d-block"><?php echo $rows['place'] ?></span>
+                                                <a href="event?e=<?php echo $rows['slug'] ?>" title="">Read more -></a>
                                         </div>
                                     </div>
-                                    <div class="mini-post-box d-flex flex-wrap align-items-center">
-                                        <a class="brd-rd5 overflow-hidden" href="product-detail.html" title=""><img
-                                                class="img-fluid" src="assets/images/resources/mini-product-img1-2.jpg"
-                                                alt="Mini Product Image 2"></a>
-                                        <div class="mini-post-info">
-                                            <h5 class="mb-0"><a href="product-detail.html" title="">Red Leather
-                                                    Jacket</a></h5>
-                                            <span
-                                                class="price scndry-clr d-block"><ins><small>$</small>50.00</ins><del><small>$</small>60.00</del></span>
-                                        </div>
-                                    </div>
-                                    <div class="mini-post-box d-flex flex-wrap align-items-center">
-                                        <a class="brd-rd5 overflow-hidden" href="product-detail.html" title=""><img
-                                                class="img-fluid" src="assets/images/resources/mini-product-img1-3.jpg"
-                                                alt="Mini Product Image 3"></a>
-                                        <div class="mini-post-info">
-                                            <h5 class="mb-0"><a href="product-detail.html" title="">Brown Cowboy Hat</a>
-                                            </h5>
-                                            <span
-                                                class="price scndry-clr d-block"><ins><small>$</small>25.00</ins><del><small>$</small>20.00</del></span>
-                                        </div>
-                                    </div>
+                                    <?php }}  ?>
+                                    
+
                                 </div>
                             </div>
 
